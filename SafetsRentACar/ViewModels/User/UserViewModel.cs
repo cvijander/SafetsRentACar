b33483c851f5db5b2b82fcc9d62a8c85460b30da
cvijander <cvija85@gmail.com>
2024-11-28 17:39:03 +0100

    validacija za username i za password

--- SafetsRentACar/ViewModels/User/UserViewModel.cs
+++ SafetsRentACar/ViewModels/User/UserViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using SafetsRentACar.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
 namespace SafetsRentACar.ViewModels.User
 {
@@ -8,11 +10,13 @@ namespace SafetsRentACar.ViewModels.User
         public int UserId { get; set; }
 
         [Required(ErrorMessage = "Ime je obavezno")]
-        [StringLength(50,ErrorMessage ="Ime ne moze imate vise od 50 karaktera.")]
+        //[StringLength(50,ErrorMessage ="Ime ne moze imate vise od 50 karaktera.")]
+        [StringLengthRangeAttribute(2,50,ErrorMessage="Ime mora da ima izmedju 2 i 50 karaktera")]
         public string FirstName { get; set; }
 
         [Required(ErrorMessage = "Prezime je obavezno")]
         [StringLength(50, ErrorMessage ="Prezime ne moze imate vise od 50 karaktera.")]
+        [StringLengthRangeAttribute(2, 50, ErrorMessage = "Prezimee mora da ima izmedju 2 i 50 karaktera")]
         public string LastName { get; set; }
 
 
@@ -33,6 +37,7 @@ namespace SafetsRentACar.ViewModels.User
         [Required(ErrorMessage = "Password  je obavezan")]
         [DataType(DataType.Password)]
         [StringLength(45,MinimumLength = 6, ErrorMessage = "Lozinka mora imati izmedju 6 i 45 karaktera")]
+        [PasswordStrengthAttribute(6,50, ErrorMessage = "Lozinka mora imati izmedju 6 i 45 karaktera, da ima i minumum 1 veliko slovo i jedan specijalan karakter")]
         public string PasswordHash { get; set; }
 
         [Required(ErrorMessage = "Potvrda passworda je obavezna")]
@@ -47,4 +52,65 @@ namespace SafetsRentACar.ViewModels.User
 
 
     }
+
+    public class StringLengthRangeAttribute : ValidationAttribute
+    {
+        private readonly int _minLength;
+        private readonly int _maxLength;
+        
+        public StringLengthRangeAttribute(int minLength, int maxLength)
+        {
+            _minLength = minLength;
+            _maxLength = maxLength;
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+           if(value is string stringUsername)
+            {
+                if((stringUsername.Length < _minLength) || (stringUsername.Length > _maxLength))
+                        {
+                    return new ValidationResult(ErrorMessage ?? $"Duzina username mora da bude veca od {_minLength}, takodje mora biti i manja od {_maxLength}");
+
+                }
+            }
+
+           return ValidationResult.Success;
+        }
+    }
+
+    public class PasswordStrengthAttribute : ValidationAttribute
+    {
+        private readonly int _minLength;
+        private readonly int _maxLength;
+
+        public PasswordStrengthAttribute(int minLenght, int maxLength)
+        {
+            _minLength = minLenght;
+            _maxLength = maxLength;
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+           if(value is string password)
+            {
+                if(password.Length < _minLength || password.Length > _maxLength)
+                {
+                    return new ValidationResult(ErrorMessage ?? $"Duzina passworda mora da bude veca od {_minLength}, takodje mora biti i manja od {_maxLength}");
+                }
+
+                if(!Regex.IsMatch(password, @"[!@#$%^&*(),.?""{}|<>]"))
+                {
+                    return new ValidationResult(ErrorMessage ?? "Password mora da sadrzi barem jedan specijalan karakter");
+                }
+
+                if(!Regex.IsMatch(password, @"[A-Z]"))
+                {
+                    return new ValidationResult(ErrorMessage ?? "Password mora da sadrzi barem jedno veliko slovo");
+                }
+            }
+           return ValidationResult.Success;
+        }
+    }
 }
+
